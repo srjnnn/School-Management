2c4345d08a299b54213c86f08b6edd909733d4d4
danielthapa <dtdaniel27@gmail.com>
2024-09-24 15:57:56 +0545

    note componennt

---
+++ src/components/Note/index.js
@@ -0,0 +1,20 @@
+class Note extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+            <div class="note-container">
+                This is a note
+            </div>
+        `;
+  }
+}
+
+customElements.define("custom-note", Note);
