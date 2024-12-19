f66fbca41b4b87913612bdc86a8bcb8b318e62e5
srjnn <mrsrjn224@gmail.com>
2024-12-19 10:54:44 +0545

    completed the whole page now yet to add the other helping pages

--- src/components/pages/Timetable/index.js
+++ src/components/pages/Timetable/index.js
@@ -17,15 +17,21 @@ class TimetablePage extends HTMLElement{
     this.addDetails();
   }
   addTable(){
-    const tableContainer = this.shadowRoot.querySelector('.tableConainer');
+    const tableContainer = this.shadowRoot.querySelector('.tableContainer');
     const table = document.createElement('my-table');
     tableContainer.appendChild(table);
   }
   addNotes(){
     const notesContainer = this.shadowRoot.querySelector('.classNotesContainer');
+    const notes = document.createElement('my-notes');
+    notesContainer.appendChild(notes);
+    notes.setAttr({height : 'auto', width: '50rem'});
+    const adjuster = notes.shadowRoot.querySelector('.adjuster');
   }
   addDetails(){
     const detailsContainer = this.shadowRoot.querySelector('.detailsContainer');
+    const details = document.createElement('my-classdetails');
+    detailsContainer.appendChild(details);
   }
 }
 const timetablePage = customElements.define('timetable-page', TimetablePage);
